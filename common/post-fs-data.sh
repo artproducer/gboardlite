1825913d7d07ee1504746078b8d85d404079ea33
artproducer <sheyeike@gmail.com>
2024-05-19 22:28:12 -0500

    beta 0.1

---
+++ common/post-fs-data.sh
@@ -0,0 +1,37 @@
+#!/system/bin/sh
+MODDIR=${0%/*}
+INFO=/data/adb/modules/.openwebview_mulch-files
+MODID=openwebview_mulch
+LIBDIR=/system
+MODPATH=/data/adb/modules/openwebview_mulch
+MODDIR="${0%/*}"
+API=$(grep_prop ro.build.version.sdk)
+CONFIG_FILE="$MODDIR/.webview"
+OVERLAY_LIST="/data/system/overlays.xml"
+RESET=$(grep "RESET=" ${CONFIG_FILE} | cut -d"=" -f2)
+OVERLAY_PATH=$(grep "OVERLAY_PATH=" ${CONFIG_FILE} | cut -d"=" -f2)
+OVERLAY_APK_FILE=$(grep "OVERLAY_APK_FILE=" ${CONFIG_FILE} | cut -d"=" -f2)
+VW_OVERLAY_PACKAGE=$(grep "VW_OVERLAY_PACKAGE=" ${CONFIG_FILE} | cut -d"=" -f2)
+if [[ $API -lt 27 ]]; then
+	STATE=3
+else
+	STATE=6
+fi
+if [[ $RESET -eq 1 ]]; then
+	# clear cache
+	rm -rf /data/resource-cache/* /data/dalvik-cache/* /cache/dalvik-cache/* /data/system/package_cache/*
+	# remove conflict
+	sed -i "/com*webview/d" /data/system/packages.list
+	sed -i "/com*webview/d" /data/system/packages.xml
+	sed -i "/com.linuxandria.WebviewOverlay/d" $OVERLAY_LIST
+	sed -i "/com.linuxandria.android.webviewoverlay/d" $OVERLAY_LIST
+	# register overlay
+	sed -i "/item packageName=\"${VW_OVERLAY_PACKAGE}\"/d" $OVERLAY_LIST
+	sed -i "s|</overlays>|    <item packageName=\"${VW_OVERLAY_PACKAGE}\" userId=\"0\" targetPackageName=\"android\" baseCodePath=\"${OVERLAY_PATH}/${OVERLAY_APK_FILE}\" state=\"${STATE}\" isEnabled=\"true\" isStatic=\"true\" priority=\"9999\" /></overlays>|" $OVERLAY_LIST
+	sed -i "s/RESET=1/RESET=0/" $CONFIG_FILE
+fi
+RESET=$(grep "RESET=" ${CONFIG_FILE} | cut -d"=" -f2)
+if [[ $RESET -eq 1 ]]; then
+	sed -Ei 's/^description=(\[.*][[:space:]]*)?/description=[ ⛔ Module is not working! Try reinstalling the module ] /g' "$MODDIR/module.prop"
+fi
+
---
+++ common/uninstall.sh
@@ -0,0 +1,24 @@
+#!/system/bin/sh
+MODDIR=${0%/*}
+INFO=/data/adb/modules/.openwebview_mulch-files
+MODID=openwebview_mulch
+LIBDIR=/system
+MODPATH=/data/adb/modules/openwebview_mulch
+if [ -f $INFO ]; then
+  while read LINE; do
+    if [ "$(echo -n $LINE | tail -c 1)" == "~" ]; then
+      continue
+    elif [ -f "$LINE~" ]; then
+      mv -f $LINE~ $LINE
+    else
+      rm -f $LINE
+      while true; do
+        LINE=$(dirname $LINE)
+        [ "$(ls -A $LINE 2>/dev/null)" ] && break 1 || rm -rf $LINE
+      done
+    fi
+  done < $INFO
+  rm -f $INFO
+fi
+
+
